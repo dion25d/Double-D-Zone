f720afda5736b48d73e5f312f776fb1b50a140b4
dion25d <dionisiopascoal123@gmail.com>
2025-05-11 09:37:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de um código simples para interatividade
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Sua mensagem foi enviada!');
+});
