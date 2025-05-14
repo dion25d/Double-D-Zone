9c21beef9340bfe7c9ec5f3e50c5bf08707871ec
dion25d <dionisiopascoal123@gmail.com>
2025-05-14 11:58:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Exemplo de um código simples para interatividade
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Sua mensagem foi enviada!');
-});
